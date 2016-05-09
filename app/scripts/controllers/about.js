4a1e2b5bae32557da242e251b8980e7deb57afdd
Arkadiusz Putko <arkadiusz.putko@gmail.com>
2016-05-09 16:24:24 +0200

    init repo

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name infoshareAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the infoshareAppApp
+ */
+angular.module('infoshareAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name infoshareAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the infoshareAppApp
+ */
+angular.module('infoshareAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
