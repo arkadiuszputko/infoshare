8b7b05435ca91550f20f4345a00bd33ab3335b67
Arkadiusz Putko <arkadiusz.putko@gmail.com>
2016-05-13 18:11:11 +0200

    added new files

---
+++ app/scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name infoshareAppApp
+ * @description
+ * # infoshareAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('infoshareAppApp', [
+    'ngCookies',
+    'ngResource'
+  ]);
