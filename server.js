f5f4a2b21ded0bbdc97c2c27e59edad9938f7ca7
Arkadiusz Putko <arkadiusz.putko@gmail.com>
2016-05-13 17:04:53 +0200

    bower for heroku

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ var app = express();
 var port = process.env.PORT || 8080;
 
 // make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/public'));
 
 // set the home page route
-app.get('/', function(req, res) {
+app.get('/dist', function(req, res) {
 
   // ejs render automatically looks in the views folder
   res.render('index');
